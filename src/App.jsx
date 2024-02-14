d7e7589c81dd6b59487da51d548888b70f155678
kirupalak <159983640+kirupalak@users.noreply.github.com>
2024-02-14 11:56:55 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Chatbot from "./components/chatbot"
-
+import Chatbot from "./components/Chatbot"
 
 function App() {
 
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" Component={Chatbot} />
+          <Route path="/" Component={Chatbot}/>
         </Routes>
       </BrowserRouter>
   )
